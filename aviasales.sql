a24714ae2b96d642165a8fc51db9e664fcc1b5d1
PripRamzak <geru7sha@gmail.com>
2024-12-02 17:45:44 +0300

    backup for lab 3

--- aviasales.sql
+++ aviasales.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: localhost
--- Время создания: Ноя 15 2024 г., 20:48
+-- Время создания: Дек 02 2024 г., 15:44
 -- Версия сервера: 10.4.32-MariaDB
 -- Версия PHP: 8.2.12
 
@@ -20,6 +20,22 @@ SET time_zone = "+00:00";
 --
 -- База данных: `aviasales`
 --
+
+DELIMITER $$
+--
+-- Процедуры
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `create_ticket` (IN `_departure_time` DATETIME, IN `_arrival_time` DATETIME, IN `_price` DECIMAL, IN `_departure_city_id` INT, IN `_arrival_city_id` INT, IN `_airline_id` INT)  MODIFIES SQL DATA INSERT INTO tickets(departure_time, arrival_time, price, departure_city_id, arrival_city_id, airline_id)
+VALUES (_departure_time, _arrival_time, _price, _departure_city_id, _arrival_city_id, _airline_id)$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `priority_tickets` (IN `_user_id` INT)   SELECT t.* from tickets as t
+LEFT JOIN (SELECT tickets.airline_id, count(*) as quan FROM orders
+JOIN tickets on orders.ticket_id = tickets.id
+WHERE user_id = _user_id
+GROUP BY tickets.airline_id) as popular_airlines on t.airline_id = popular_airlines.airline_id
+ORDER BY quan DESC$$
+
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -68,6 +84,7 @@ INSERT INTO `cities` (`id`, `name`) VALUES
 (2, 'Москва'),
 (3, 'Париж'),
 (10, 'Пекин'),
+(19, 'Рио-де-Жанейро'),
 (18, 'Рофланч');
 
 -- --------------------------------------------------------
@@ -113,7 +130,7 @@ CREATE TABLE `dispatcher_requests` (
 --
 
 INSERT INTO `dispatcher_requests` (`id`, `surname`, `firstname`, `middlename`, `status`, `user_id`, `airline_id`) VALUES
-(1, 'Титович', 'Андрей', 'Петрович', 'approved', 7, 2);
+(2, 'тест', 'тест', '', 'denied', 2, 4);
 
 --
 -- Триггеры `dispatcher_requests`
@@ -150,7 +167,10 @@ INSERT INTO `orders` (`id`, `firstname`, `surname`, `middlename`, `status`, `use
 (1, 'Герман', 'Сухой', 'Андреевич', 'approved', 2, 6),
 (2, 'Владислав', 'Павличук', 'Владимирович', 'denied', 2, 10),
 (3, 'Герман', 'Сухой', 'Андреевич', 'pending', 2, 11),
-(4, 'Алексей', 'Сивец', 'Игорьевич', 'pending', 7, 10);
+(4, 'Алексей', 'Сивец', 'Игорьевич', 'pending', 7, 10),
+(6, 'Пользователь', 'Новый', '', 'approved', 9, 10),
+(10, 'Перец', 'Крутой', '', 'pending', 7, 12),
+(11, 'Крутыч', 'Перцовый', '', 'pending', 7, 12);
 
 -- --------------------------------------------------------
 
@@ -173,17 +193,20 @@ CREATE TABLE `tickets` (
 --
 
 INSERT INTO `tickets` (`id`, `departure_time`, `arrival_time`, `price`, `departure_city_id`, `arrival_city_id`, `airline_id`) VALUES
-(1, '2024-10-23 12:16:00', '2024-10-24 01:09:00', 1200, 1, 8, 2),
-(2, '2024-10-30 13:00:00', '2024-10-30 15:55:00', 255, 1, 2, 2),
+(1, '2024-12-23 12:16:00', '2024-12-24 06:09:00', 1200, 1, 8, 2),
+(2, '2026-11-10 14:00:00', '9999-10-30 09:09:00', 100, 1, 2, 2),
 (3, '2024-10-21 00:25:00', '2024-10-21 14:08:00', 980, 2, 10, 4),
 (6, '2024-11-17 10:11:00', '2024-11-17 17:43:00', 501, 1, 6, 2),
-(7, '9999-09-08 11:01:00', '9999-09-09 09:09:00', 33333333, 5, 8, 2),
+(7, '9999-09-08 11:01:00', '9999-09-09 09:09:00', 33333333, 7, 8, 2),
 (8, '3333-11-11 03:33:00', '9999-09-09 09:09:00', 666, 5, 8, 2),
 (9, '2025-02-22 04:33:00', '2025-03-23 05:55:00', 40, 16, 5, 2),
 (10, '2024-12-08 14:07:00', '2024-12-08 16:08:00', 350, 1, 2, 2),
 (11, '2024-12-12 14:07:00', '2024-12-17 19:09:00', 100, 5, 7, 4),
 (12, '2222-09-09 11:13:00', '3293-03-09 04:24:00', 666, 16, 5, 10),
-(13, '2024-12-14 19:00:00', '2024-12-16 11:11:00', 150, 8, 7, 2);
+(13, '2024-12-14 19:00:00', '9999-09-09 09:09:00', 150, 8, 7, 2),
+(14, '9999-09-09 09:09:00', '9999-09-09 09:09:00', 100, 5, 8, 2),
+(15, '8888-08-08 08:08:00', '9999-09-09 09:09:00', 3333, 5, 8, 2),
+(16, '2024-12-13 14:00:00', '2024-12-13 23:59:00', 400, 1, 3, 2);
 
 -- --------------------------------------------------------
 
@@ -206,7 +229,9 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `role`) VALUES
 (1, 'cool_admin', 'admin@gmail.com', '21232f297a57a5a743894a0e4a801fc3', 'admin'),
 (2, 'roflik', 'roflik@gmail.com', '202cb962ac59075b964b07152d234b70', 'user'),
-(7, 'flylover', 'flylover@gmail.com', '698d51a19d8a121ce581499d7b701668', 'user');
+(7, 'flylover', 'flylover@gmail.com', '698d51a19d8a121ce581499d7b701668', 'user'),
+(8, 'men4ik', 'men4ik@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', 'user'),
+(9, 'newuser', 'user@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', 'user');
 
 --
 -- Индексы сохранённых таблиц
@@ -280,7 +305,7 @@ ALTER TABLE `airlines`
 -- AUTO_INCREMENT для таблицы `cities`
 --
 ALTER TABLE `cities`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT для таблицы `dispatchers`
@@ -292,25 +317,25 @@ ALTER TABLE `dispatchers`
 -- AUTO_INCREMENT для таблицы `dispatcher_requests`
 --
 ALTER TABLE `dispatcher_requests`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT для таблицы `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT для таблицы `tickets`
 --
 ALTER TABLE `tickets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
